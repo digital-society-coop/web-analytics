c0f9832fd5ae9a05dbae5c017a825542a90296a4
Endre Kadas <ekadas@gmail.com>
2024-02-12 15:17:16 +0000

    feat: Added umami kuberntes deployment

--- deploy.sh
+++ deploy.sh
@@ -11,37 +11,24 @@ function usage {
 environment=$1
 shift
 
-service=web-analytics
+export SERVICE=web-analytics
 
-echo "Deploying $service-$environment... " >&2
+echo "Deploying $SERVICE-$environment... " >&2
 echo >&2
 
-export NAMESPACE="$service-$environment"
-export MANAGED_DOMAIN="$service-$environment.$RUNTIME_DOMAIN"
-export BASE_URL="${BASE_URL:-https://$MANAGED_DOMAIN}"
-export MAILER_EMAIL="$MAILER_EMAIL"
-export SMTP_HOST_ADDR="$SMTP_HOST_ADDR"
-export SMTP_HOST_PORT="$SMTP_HOST_PORT"
-BASE64_SECRET_KEY_BASE=$(echo -n "$SECRET_KEY_BASE" | openssl base64 -A)
+export NAMESPACE="$SERVICE-$environment"
+export MANAGED_DOMAIN="$SERVICE-$environment.$RUNTIME_DOMAIN"
+BASE64_SECRET=$(echo -n "$SECRET" | openssl base64 -A)
 BASE64_POSTGRES_PASSWORD=$(echo -n "$POSTGRES_PASSWORD" | openssl base64 -A)
-BASE64_CLICKHOUSE_PASSWORD=$(echo -n "$CLICKHOUSE_PASSWORD" | openssl base64 -A)
-BASE64_SMTP_EMAIL=$(echo -n "$SMTP_EMAIL" | openssl base64 -A)
-BASE64_SMTP_PASSWORD=$(echo -n "$SMTP_PASSWORD" | openssl base64 -A)
 
-export BASE64_SECRET_KEY_BASE BASE64_POSTGRES_PASSWORD BASE64_CLICKHOUSE_PASSWORD BASE64_SMTP_EMAIL BASE64_SMTP_PASSWORD
+export BASE64_SECRET BASE64_POSTGRES_PASSWORD
 
 # shellcheck disable=SC2016
-variables='$NAMESPACE
+variables='$SERVICE
+  $NAMESPACE
   $MANAGED_DOMAIN
-  $BASE_URL
-  $MAILER_EMAIL
-  $SMTP_HOST_ADDR
-  $SMTP_HOST_PORT
-  $BASE64_SECRET_KEY_BASE
-  $BASE64_CLICKHOUSE_PASSWORD
-  $BASE64_POSTGRES_PASSWORD
-  $BASE64_SMTP_EMAIL
-  $BASE64_SMTP_PASSWORD'
+  $BASE64_SECRET
+  $BASE64_POSTGRES_PASSWORD'
 
 envsubst "$variables" <kubernetes.yaml | kubectl apply -f -
 
